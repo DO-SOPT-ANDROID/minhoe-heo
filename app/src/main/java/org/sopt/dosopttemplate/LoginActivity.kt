1f166abfdfc08f6a7e092d2ce4041149fa352ab9
heominhoe <hhmmm1005@naver.com>
2023-10-13 03:18:52 +0900

    [passward]:inputType 이용해서 숨김처리

---
+++ app/src/main/java/org/sopt/dosopttemplate/LoginActivity.kt
@@ -0,0 +1,64 @@
+package org.sopt.dosopttemplate
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.dosopttemplate.databinding.SignInBinding
+
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: SignInBinding
+    private lateinit var activityResult: ActivityResultLauncher<Intent>
+    lateinit var id: String
+    lateinit var pw: String
+    lateinit var nickname: String
+    lateinit var mbti: String
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in)
+        binding = SignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setResult()
+
+        binding.signUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            activityResult.launch(intent)
+        }
+
+        binding.signIn.setOnClickListener {
+            if (binding.etSignInId.text.toString() == id && binding.etSignInPw.text.toString() == pw) {
+                Toast.makeText(this, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, MainActivity::class.java).apply {
+                    putExtra("id", id)
+                    putExtra("mbti", mbti)
+                    putExtra("nickname", nickname)
+                    putExtra("pw", pw)
+                }
+
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+        private fun setResult() {
+            activityResult = registerForActivityResult(
+                ActivityResultContracts.StartActivityForResult()
+            ) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    id = result.data?.getStringExtra("id") ?: ""
+                    pw = result.data?.getStringExtra("pw") ?: ""
+                    nickname = result.data?.getStringExtra("nickname") ?: ""
+                    mbti = result.data?.getStringExtra("mbti") ?: ""
+                }
+            }
+        }
+    }
+
---
+++ app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
@@ -0,0 +1,36 @@
+package org.sopt.dosopttemplate
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import org.sopt.dosopttemplate.databinding.SignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: SignUpBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+        binding = SignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUp.setOnClickListener {
+            if (binding.etSignUpId.length() in 6..10 && binding.etSignUpPw.length() in 8..12
+                && binding.etSignUpName.length() >= 1 && binding.etSignUpMbti.length() >= 1
+            ) {
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.putExtra("id", binding.etSignUpId.text.toString())
+                    .putExtra("pw", binding.etSignUpPw.text.toString())
+                    .putExtra("nickname", binding.etSignUpName.text.toString())
+                    .putExtra("mbti", binding.etSignUpMbti.text.toString())
+
+                setResult(RESULT_OK, intent)
+                Toast.makeText(this, "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show()
+
+                finish()
+
+            } else
+                Toast.makeText(this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
