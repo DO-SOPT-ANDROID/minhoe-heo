59ae2ed15a5b812d6e62798a84b228d04c25f676
heominhoe <hhmmm1005@naver.com>
2023-11-08 21:01:05 +0900

    [refactor] 뷰모델 생성

--- app/src/main/java/org/sopt/dosopttemplate/HomeFragment.kt
+++ app/src/main/java/org/sopt/dosopttemplate/HomeFragment.kt
@@ -6,57 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.ConcatAdapter
 import org.sopt.dosopttemplate.databinding.FragmentHomeBinding
-
-class HomeFragment: Fragment() {
-    private var _binding : FragmentHomeBinding? = null
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
-        get() = requireNotNull(_binding){ "바인딩에러" }
+        get() = requireNotNull(_binding) { "바인딩에러" }
 
-
-    private val mockFriendList =listOf<Friend>(
-        Friend(
-            profileImage = R.drawable.img,
-            name = "라이언",
-            self_description = "",
-        ),
-        Friend(
-            profileImage = R.drawable.img_1,
-            name = "양파쿵야",
-            self_description = "돈벌기 힘들다",
-        ),
-        Friend(
-            profileImage = R.drawable.img_2,
-            name = "스누피친구",
-            self_description = "내 이름이 머더라",
-        ),
-        Friend(
-            profileImage = R.drawable.img_3,
-            name = "신짱구",
-            self_description = "어 이쁜 누나다",
-        ),
-        Friend(
-            profileImage = R.drawable.img_4,
-            name = "김철수",
-            self_description = "",
-        ),
-        Friend(
-            profileImage = R.drawable.img_5,
-            name = "흰둥이",
-            self_description = "",
-        ),
-        Friend(
-            profileImage = R.drawable.img_6,
-            name = "액션가면",
-            self_description = "하하ㅏ",
-        ),
-        Friend(
-            profileImage = R.drawable.img_7,
-            name = "부리부리대마왕",
-            self_description = "",
-        )
-    )
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -73,13 +31,13 @@ class HomeFragment: Fragment() {
         val meAdapter = MeAdapter(requireContext())
 
 
-        friendAdapter.setFriendList(mockFriendList)
+        friendAdapter.setFriendList(viewModel.mockFriendList)
 
         val concatAdapter = ConcatAdapter(meAdapter, friendAdapter)
-        binding.rvFriends.adapter= concatAdapter
+        binding.rvFriends.adapter = concatAdapter
 
-        binding.fbAddFriend.setOnClickListener{
-            Toast.makeText(requireContext(),"친구 추가를 할 수 없습니다.", Toast.LENGTH_SHORT).show()
+        binding.fbAddFriend.setOnClickListener {
+            Toast.makeText(requireContext(), "친구 추가를 할 수 없습니다.", Toast.LENGTH_SHORT).show()
         }
     }
 
