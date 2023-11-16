7dc158d7770852e05331ec512f5f180690b36a50
heominhoe <hhmmm1005@naver.com>
2023-11-17 03:10:15 +0900

    [fix] follower ui 수정

--- app/src/main/java/org/sopt/dosopttemplate/HomeFragment.kt
+++ app/src/main/java/org/sopt/dosopttemplate/HomeFragment.kt
@@ -1,20 +1,24 @@
 package org.sopt.dosopttemplate
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.ConcatAdapter
+import androidx.lifecycle.Observer
 import org.sopt.dosopttemplate.databinding.FragmentHomeBinding
+import org.sopt.dosopttemplate.follower.FollowerAdapter
+import org.sopt.dosopttemplate.follower.FollowerViewModel
+import org.sopt.dosopttemplate.response.ResponseFollowerDto
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
         get() = requireNotNull(_binding) { "바인딩에러" }
 
-    private val viewModel by viewModels<HomeViewModel>()
+    private val viewModel by viewModels<FollowerViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,24 +31,20 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val friendAdapter = FriendAdapter(requireContext())
-        val meAdapter = MeAdapter(requireContext())
 
-        friendAdapter.setFriendList(viewModel.mockFriendList)
-
-        val concatAdapter = ConcatAdapter(meAdapter, friendAdapter)
-        binding.rvFriends.adapter = concatAdapter
-
-        binding.fbAddFriend.setOnClickListener {
-            showAddFriendEditText()
-        }
+        viewModel.loadFollowerData()
+        viewModel.followerData.observe(viewLifecycleOwner, Observer {data->
+            setAdapter(data)
+            Log.d("뷰모델", data.toString())
+        })
     }
 
-    private fun showAddFriendEditText() {
-        val dialog = CustomDialog()
-        dialog.show(requireActivity().supportFragmentManager, "CustomDialog")
+    private fun setAdapter(followerList: List<ResponseFollowerDto.FollowerData>?){
+        Log.e("어댑터 연결 성공",followerList.toString())
+        val followerAdapter = FollowerAdapter()
+        binding.rvFollower.adapter = followerAdapter
+        followerAdapter.submitList(followerList)
     }
-
 
     override fun onDestroyView() {
         _binding = null
