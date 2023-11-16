89d3796e057f5bb5de148de8d3f1dc3db0e0b1e1
heominhoe <hhmmm1005@naver.com>
2023-11-17 03:14:42 +0900

    [feat] follower 데이터 만들기

---
+++ app/src/main/java/org/sopt/dosopttemplate/follower/Follower.kt
@@ -0,0 +1,10 @@
+package org.sopt.dosopttemplate.follower
+
+import androidx.annotation.DrawableRes
+
+data class Follower(
+    val id: Int,
+    val firstName: String,
+    val email: String,
+    val avatar: String
+)
