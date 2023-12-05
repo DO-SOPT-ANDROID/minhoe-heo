fe06ca37ca593f2f7cbc5908aa1e7fa5e15b3ce1
heominhoe <hhmmm1005@naver.com>
2023-12-05 22:53:39 +0900

    [FEAT] : observe 사용해서 아이디, 비밀번호 에러메세지 띄우기

--- app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
@@ -3,6 +3,7 @@ package org.sopt.dosopttemplate
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -18,9 +19,9 @@ class SignUpActivity : AppCompatActivity() {
         binding = SignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        canSignUp()
-        observeSignUpResult()
         signUp()
+        checkIdValid()
+        checkPwValid()
 
         binding.root.setOnClickListener {
             hideKeyboard()
@@ -28,6 +29,7 @@ class SignUpActivity : AppCompatActivity() {
 
         binding.lifecycleOwner = this
         binding.authSignUpViewModel = authSignUpViewModel
+
     }
 
     private fun hideKeyboard() {
@@ -42,57 +44,24 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
-    private fun canSignUp() {
-        binding.signUp.setOnClickListener {
-            val id = binding.etSignUpId.text.toString()
-            val password = binding.etSignUpPw.text.toString()
-
-//            val setId = id.matches("[a-zA-Z]+[0-9]+".toRegex())
-//            val setPw = password.matches("(?=.*[a-zA-Z])(?=.*\\d).+".toRegex())
-
-            if (binding.etSignUpId.length() in 6..10 && binding.etSignUpPw.length() in 6..12 && validateId(
-                    id
-                ) &&
-                validatePassword(password) && !binding.etSignUpName.text.isNullOrBlank() && !binding.etSignUpMbti.text.isNullOrBlank()
-            ) {
-                intent = Intent(this, LoginActivity::class.java)
-                    .putExtra("id", binding.etSignUpId.text.toString())
-                    .putExtra("pw", binding.etSignUpPw.text.toString())
-                    .putExtra("nickname", binding.etSignUpName.text.toString())
-                    .putExtra("mbti", binding.etSignUpMbti.text.toString())
-
-                setResult(RESULT_OK, intent)
-                Toast.makeText(this, "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show()
-
-                finish()
-            } else
-                Toast.makeText(this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+    private fun checkIdValid() {
+        authSignUpViewModel.username.observe(this) { id ->
+            Log.e("id", "${id}")
+            if (id.isNullOrBlank() || authSignUpViewModel.isIdValid()) {
+                binding.tvSignUpId.error = null
+            } else if (!authSignUpViewModel.isIdValid()) {
+                binding.tvSignUpId.error = "영문, 숫자를 포함해 6-10자 이내로 쓰십시오"
+            }
         }
     }
 
-    fun validateId(id: String): Boolean {
-        val idPattern = "^[a-z0-9]{6,}\$".toRegex()
-        return idPattern.matches(id)
-    }
-
-    fun validatePassword(password: String): Boolean {
-        val passwordPattern =
-            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+\$).{6,}\$".toRegex()
-        return passwordPattern.matches(password)
-    }
-
-    private fun observeSignUpResult() {
-        authSignUpViewModel.signUpSuccess.observe(this) {
-            if (it) {
-                Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
-                startActivity(
-                    Intent(
-                        this,
-                        LoginActivity::class.java,
-                    )
-                )
-            } else {
-                Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+    private fun checkPwValid() {
+        authSignUpViewModel.password.observe(this) { password ->
+            Log.e("password", "${password}")
+            if (password.isNullOrBlank() || authSignUpViewModel.isPwValid()) {
+                binding.tvSignUpPw.error = null
+            } else if (!authSignUpViewModel.isPwValid()) {
+                binding.tvSignUpPw.error = "영문,숫자,특수문자를 포함해 6-12자 이내로 쓰십시오"
             }
         }
     }
@@ -102,15 +71,33 @@ class SignUpActivity : AppCompatActivity() {
             val id = binding.etSignUpId.text.toString()
             val password = binding.etSignUpPw.text.toString()
             val nickname = binding.etSignUpName.text.toString()
-            val mbti = binding.etSignUpMbti.text.toString()
+
+            Log.e("사용자 정보", "${id}, ${password}, ${nickname}")
 
             authSignUpViewModel.signUp(
-                id = id,
+                username = id,
                 password = password,
-                nickname = nickname,
-                mbti = mbti
+                nickname = nickname
             )
+
+            authSignUpViewModel.signUpSuccess.observe(this) {
+                Log.e("signup", "${it}")
+                if (it) {
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                    startActivity(
+                        Intent(
+                            this,
+                            LoginActivity::class.java,
+                        )
+                    )
+                    Log.e("btn", "넘어가나?")
+                } else {
+                    Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
 
         }
     }
 }
+
+
