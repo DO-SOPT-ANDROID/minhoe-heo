5d702eb564ce531e560c3af155c4774d06ca7251
heominhoe <hhmmm1005@naver.com>
2023-12-19 19:32:05 +0900

    [ADD] : interceptor 추가

--- app/src/main/java/org/sopt/dosopttemplate/ApiFactory.kt
+++ app/src/main/java/org/sopt/dosopttemplate/ApiFactory.kt
@@ -1,13 +1,29 @@
 package org.sopt.dosopttemplate
 
+import android.util.Log
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.sopt.dosopttemplate.follower.FollowerService
 import retrofit2.Retrofit
 
 object ApiFactory {
     private const val BASE_URL = BuildConfig.AUTH_BASE_URL
+
+    private fun getLogOkHttpClient(): Interceptor {
+        val loggingInterceptor = HttpLoggingInterceptor { message ->
+            Log.d("Retrofit2", "CONNECTION INFO -> $message")
+        }
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return loggingInterceptor
+    }
+
+    val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(getLogOkHttpClient())
+        .build()
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
